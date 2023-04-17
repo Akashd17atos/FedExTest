99becba04afc199cb856f697202526351b1cc11c
Akash Dudhate <akash.dudhate@atos.net>
2023-04-17 17:15:55 +0530

    updated code

--- read_gtiapi.py
+++ read_gtiapi.py
@@ -1,16 +1,18 @@
-import requests
+import shlex
+import subprocess
+import json
 
-"""
-API_URL = "https://api.havenondemand.com/1/api/async/recognizelicenseplates/v1"
+def call_curl(curl):
+    args = shlex.split(curl)
+    process = subprocess.Popen(args, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    return json.loads(stdout.decode('utf-8'))
 
-data = {
-    "url": "https://www.havenondemand.com/sample-content/videos/gb-plates.mp4",
-    "source_location": "GB",
-    "apikey": "695e513c-xxxx-xxxx-a666-xxxxxxxxxx"
- }
 
-response = requests.post(API_URL, data)
-print(response.json())
-"""
-
-print("This is test")
+if __name__ == '__main__':
+    curl = '''curl - X
+    POST - d
+    '{"nw_src": "10.0.0.1/32", "nw_dst": "10.0.0.2/32", "nw_proto": "ICMP", "actions": "ALLOW", "priority": "10"}'
+    http: // localhost: 8080 / firewall / rules / 0000000000000001 '''
+    output = call_curl(curl)
+    print(output)
